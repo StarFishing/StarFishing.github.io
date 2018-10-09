65a8e23b911d253917bfe5070e1a985812804063
StarFishing <2923627211@qq.com>
2018-10-09 22:21:51 +0800

    Site updated: 2018-10-09 22:21:47

--- introduction/js/functions.js
+++ introduction/js/functions.js
@@ -1,7 +1,16 @@
 jQuery(function($){ "use strict";
 
 
+// //页面加载完成后显示
+document.onreadystatechange=function () {
+      if(document.readyState=="complete")
+      {
+        $(".loading").fadeOut();
+      }
+    }
 //scroll sections
+
+
 	$(".scroll").click(function(event){
 					
 				event.preventDefault();
