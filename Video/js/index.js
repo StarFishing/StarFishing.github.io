a7d0eeaffb8507dce58b15c06ce5b0ef56eef28e
StarFishing <2923627211@qq.com>
2018-10-29 13:06:32 +0800

    Site updated: 2018-10-29 13:06:26

--- Video/js/index.js
+++ Video/js/index.js
@@ -1,24 +1,45 @@
 $(function(){
     var input=$(".text");
-    var button=$(".button");
+    var button=$(".search .button");
+    var interface=$(".Cinterface .button");
     var remaind=$(".remaind");
     var know=$(".know");
+    var content=$(".content");
+    var interfaceUrl=["http://jx.598110.com/v/3.php?url=","http://www.1717yun.com/jx/ty.php?url="];
+    var href="http://jqaaa.com/jq3/?url=&url=";
+    var textinner=["我查个锤子，你都没输网址","搞毛啊，都不输入","又点人家，快说想看什么"];
+    var count=0;
     remaind.hide();
     know.click(function(){
         remaind.slideUp();
     });
     button.click(function(){
-        // if(!input)
-        //      alert("input");
-        //  else
         var text=input.val();
         if(!text)
-        // remaind.fadeIn();
-        remaind.slideDown();
+        {
+             content.text(textinner[count]);
+             var num=count+1;
+             count=getcount(num);
+             remaind.slideDown();
+        }
         else
         {
-             window.location.href = 'http://jqaaa.com/jq3/?url=&url='+text;
-             // window.open('http://jqaaa.com/jq3/?url=&url='+text);
+            var url=href+text
+             // window.location.href = 'http://jqaaa.com/jq3/?url=&url='+text;
+             window.open(url);
         }
     });
+    interface.click(function(){
+        remaind.slideUp();
+        var url=$(this).attr("id");
+        href=interfaceUrl[url-1];
+        content.text("接口"+url+"更换成功");
+        remaind.slideDown();
+
+    });
+    function getcount(num){
+        if(num>2)
+            num=0;
+        return num;
+    }
 });
