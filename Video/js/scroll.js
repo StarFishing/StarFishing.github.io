a7a1f18867b6511d6d97553190db22bdf6a85ee1
StarFishing <2923627211@qq.com>
2018-10-30 10:06:50 +0800

    Site updated: 2018-10-30 10:06:44

--- Video/js/scroll.js
+++ Video/js/scroll.js
@@ -3,3 +3,7 @@ var menuScroll;
 window.onload=function(){
     var menuScroll = new BScroll(wrapper, {click:true});
 };
+window.onresize=function(){
+    if(menuScroll)
+    menuScroll.refresh();
+}
