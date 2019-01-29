ae3c50b05b134c64b4483a07b534ad023c7edc6f
StarFishing <2923627211@qq.com>
2019-01-29 23:04:32 +0800

    Site updated: 2019-01-29 23:04:26

--- test02/js/scroll.js
+++
@@ -1,9 +0,0 @@
-var wrapper=document.getElementsByClassName("wrapper")[0];
-var menuScroll;
-window.onload=function(){
-    var menuScroll = new BScroll(wrapper, {click:true});
-};
-window.onresize=function(){
-    if(menuScroll)
-    menuScroll.refresh();
-}
